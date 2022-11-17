901f2953c4f7f22017bc498b21b571a12f66c2cc
unknown <muflihmaxi@gmail.com>
2022-11-17 11:28:49 +0700

    NYOOS DEAFEN

--- lib/form.dart
+++ lib/form.dart
@@ -133,6 +133,31 @@ class _MyFormPageState extends State<MyFormPage> {
                     judul.add(_judul);
                     nominal.add(_nominal);
                     tipe.add(_tipe);
+                    showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 15,
+                    child: ListView(
+                      padding: const EdgeInsets.only(top: 20, bottom: 20),
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        const Center(child: Text('Data telah berhasil disimpan :D')),
+                        const SizedBox(height: 20),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Kembali'),
+                        ), 
+                      ],
+                    ),
+                  );
+                },
+              );
                   }
                 },
                 child: const Text(
